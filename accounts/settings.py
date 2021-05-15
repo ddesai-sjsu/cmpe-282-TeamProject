701fcc1ad4dffb55281184398d8519e04f6b4b0d
ANANTHUPADHYA <ananthupadhya10@gmail.com>
2021-05-14 17:57:05 -0700

    Uncommented env variables

--- accounts/settings.py
+++ accounts/settings.py
@@ -1,15 +1,14 @@
 import sys
 import os
 
-# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
-# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
-# AWS_REGION = os.getenv("AWS_REGION", None)
-# COGNITO_USER_POOL_ID = os.getenv("COGNITO_USER_POOL_ID", None)
-# COGNITO_APP_CLIENT_ID = os.getenv("COGNITO_APP_CLIENT_ID", None)
-# S3_BUCKET = os.getenv("S3_BUCKET", None)
-# S3_URL = os.getenv("S3_BUCKET", None)
-# CLOUD_FRONT_URL = os.getenv("CLOUD_FRONT_URL", None)
-
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+AWS_REGION = os.getenv("AWS_REGION", None)
+COGNITO_USER_POOL_ID = os.getenv("COGNITO_USER_POOL_ID", None)
+COGNITO_APP_CLIENT_ID = os.getenv("COGNITO_APP_CLIENT_ID", None)
+S3_BUCKET = os.getenv("S3_BUCKET", None)
+S3_URL = os.getenv("S3_BUCKET", None)
+CLOUD_FRONT_URL = os.getenv("CLOUD_FRONT_URL", None)
 
 # HOST_NAME = os.getenv("HOST_NAME", "localhost")
 # HOST_PORT = os.getenv("HOST_PORT", "8000")
